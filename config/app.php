118e56b528df3c573ccebd2356303f7ad64b5e26
mtaopp <m.ta.opportunity@gmail.com>
2021-10-22 18:42:39 +0200

    added Fortify service provider to conig/app.php

--- config/app.php
+++ config/app.php
@@ -177,6 +177,7 @@ return [
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
         Laravel\Fortify\FortifyServiceProvider::class,
+        App\Providers\FortifyServiceProvider::class,
     ],
 
     /*
